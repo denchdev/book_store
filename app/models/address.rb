941d9aedf13019061fdb575183a533c77b5ea440
denromani4 <denromani4@gmail.com>
2015-07-23 17:03:05 +0300

    Update address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,5 @@ class Address < ActiveRecord::Base
   belongs_to :country
 
   validates_presence_of :address, :zipcode, :city, :phone
+  validates :zipcode, format: {with: /\A[0-9]{5}\z/, message: "should contain 5 digits"}
 end
