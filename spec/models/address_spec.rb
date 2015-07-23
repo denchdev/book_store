59acc51bcbb49d343c53e8d5ee9a3bbeba327a75
denromani4 <denromani4@gmail.com>
2015-07-23 17:28:30 +0300

    Update address_spec.rb

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Address, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:country) { FactoryGirl.create(:country) }
+  let(:address) { FactoryGirl.create(:address, country: country) }
+
+  context "associations" do
+    it { expect(address).to belong_to(:country) }
+    it { expect(address).to belong_to(:order) }
+    it { expect(address).to belong_to(:customer) }
+  end
+  context "validations" do
+    it { expect(address).to validate_presence_of(:city) }
+    it { expect(address).to validate_presence_of(:phone) }
+    it { expect(address).to validate_presence_of(:zipcode) }
+    it { expect(address).to validate_presence_of(:address) }
+    it { expect(address).to validate_presence_of(:country) }
+    it { expect(address).to allow_value('12345').for(:zipcode).with_message("should contain 5 digits")}
+    it { expect(address).to_not allow_value('1234').for(:zipcode).with_message("should contain 5 digits")}
+    it { expect(address).to_not allow_value('abcde').for(:zipcode).with_message("should contain 5 digits")}
+  end
 end
