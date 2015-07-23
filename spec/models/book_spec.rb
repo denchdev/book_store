5510ba23865ba091e60a64aa3927c0781d309657
denromani4 <denromani4@gmail.com>
2015-07-23 17:29:24 +0300

    Update book_spec.rb

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:author) { FactoryGirl.create(:author) }
+
+  context "associations" do
+    it { expect(author).to have_many(:books) }
+  end
+  context "validations" do
+    it { expect(author).to validate_presence_of(:firstname) }
+    it { expect(author).to validate_presence_of(:lastname) }
+  end
+  context ".full_name" do
+    it "shows firstname and lastname of the author properly" do
+      expect(author.full_name).to eq("John Tolkien")
+    end
+  end
+  context ".number_of_books" do
+    it "shows number of all author's books" do
+      author_book = FactoryGirl.create(:book, author_id: author.id)
+      expect(author.number_of_books).to eq(1)
+    end
+  end
 end
