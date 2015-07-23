88c62bcf8c3e3acbb0f713412d6b8525357259a6
denromani4 <denromani4@gmail.com>
2015-07-23 17:24:38 +0300

    Update order_item.rb

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,4 +3,33 @@ class OrderItem < ActiveRecord::Base
   belongs_to :book
 
   validates_presence_of :price, :quantity 
+  
+  validate :if_in_stock
+  #validates :quantity, presence: true, numericality: { greater_than: 0 }
+  before_save :count_price!
+  scope :in_cart_with, ->(book) {where('book_id = ?', book)}
+
+  def count_price!
+    self.price = (book.price * quantity).to_f
+  end
+
+  def increase_quantity!(number)
+    self.quantity += number.to_i
+    save
+  end
+
+  def add_to_order!(book, quantity, order)
+    if Order.find(order.to_i).order_items.in_cart_with(book).empty?
+      self.update(book_id: book, quantity: quantity, order_id: order.to_i)
+    else
+      OrderItem.find_by(order_id: order.to_i, book_id: book).increase_quantity!(quantity)
+    end
+  end
+
+  private 
+  def if_in_stock
+    unless book.in_stock.to_i >= quantity.to_i
+      errors.add(:book, 'not in stock!')
+    end
+  end
 end
