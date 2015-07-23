66f21e3c5bef5ace070464e56bf70857978ad056
denromani4 <denromani4@gmail.com>
2015-07-23 17:13:59 +0300

    Update customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,12 @@
 class Customer < ActiveRecord::Base
-  has_many :orders
-  has_many :ratings  
+  has_many :orders, dependent: :destroy
+  has_many :ratings, dependent: :destroy  
   #has_many :books, through: ratings
+  has_many :orders, 
+  #has_one :customer_bill_address, class_name: 'CustomerBillAddress', dependent: :destroy
+  #has_one :customer_ship_address, class_name: 'CustomerShipAddress', dependent: :destroy
+  #has_many :order_items, dependent: :destroy
+  
 
   validates_presence_of :firstname, :lastname, :email, :password
   validates :email, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
