1ae0a1dbad46ce8ee1fdca257c8ccb234534339f
denromani4 <denromani4@gmail.com>
2015-07-23 17:25:58 +0300

    Update rating.rb

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,4 +3,17 @@ class Rating < ActiveRecord::Base
   belongs_to :customer
 
   validates_inclusion_of :rating, :in => 0..100
+  
+  belongs_to :customer
+  belongs_to :book
+  
+  after_save :calculate_book_avg_rating
+
+  def already_rated?
+    Rating.where(customer_id: customer.id, book_id: book.id).empty?
+  end
+
+  def calculate_book_avg_rating
+    book.recalculate_avg_rating!
+  end
 end
