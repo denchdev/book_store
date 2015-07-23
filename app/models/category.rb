e0706a5031e2cc922e422eed06c6b78e158f8879
denromani4 <denromani4@gmail.com>
2015-07-23 17:08:52 +0300

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ActiveRecord::Base
   has_many :books
 
   validates :title, presence: true, uniqueness: true
+  
+  def number_of_books
+    Book.where(category_id: self.id).count
+  end
 end
