d9e6720b0e278b116ef54292020cd8a8ccb7a756
denromani4 <denromani4@gmail.com>
2015-07-23 17:04:31 +0300

    Update author.rb

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,12 @@ class Author < ActiveRecord::Base
   has_many :books
 
   validates_presence_of :firstname, :lastname
+  
+  def full_name
+    firstname + " " + lastname
+  end
+
+  def number_of_books
+    Book.where(author_id: self.id).count
+  end
 end
